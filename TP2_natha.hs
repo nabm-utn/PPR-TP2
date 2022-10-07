92a566f6ff822828cb411a3ce0f4c797325f16b2
Nathaniel Balderramas <83505517+nabm-utn@users.noreply.github.com>
2022-10-07 10:25:03 -0300

    Updated Natha's Version

--- TP2_natha.hs
+++ TP2_natha.hs
@@ -7,7 +7,7 @@ función deberá devolver False.
 -}
 esDivisible :: (Int, Int) -> Bool
 esDivisible(a, b) | (a `rem` b) == 0 = True
-				  | otherwise = False
+                  | otherwise = False
 
 
 {-
@@ -17,15 +17,17 @@ generar una lista con todos los números enteros mediante los cuales el número 
 incluyendo al 1 y al propio número n entre tales factores. Realizar las validaciones necesarias. Utilizar
 recursividad. Reutilizar la función de la consigna 1).
 -}
+
 listaFactoresDivisiblesDe :: Int -> [Int]
 listaFactoresDivisiblesDe n = listaDivisores(n, n)
 
 listaDivisores :: (Int, Int) -> [Int]
-listaDivisores (a, b) 	| b == 0					 = []
-						| b == 1 					 = [1]
-						| esDivisible(a, b) == True  = b:listaDivisores(a, b-1)
-						| esDivisible(a, b) == False = listaDivisores(a, b-1)
-						| otherwise = []
+listaDivisores (a, 0) = []
+listaDivisores (a, 1) = [1]
+listaDivisores (a, b) = if esDivisible(a, b) 
+                        then b:listaDivisores(a, b-1)
+                        else listaDivisores(a, b-1)
+
 {-
 3)
 Implementar una función en Haskell que reciba un número entero nro. La función
@@ -35,35 +37,39 @@ función de la consigna 1).
 -}
 
 esPrimo :: Int -> Bool
-esPrimo n 	| length(listaFactoresDivisiblesDe n) == 2 = True
-			| otherwise = False
+esPrimo n   | length(listaFactoresDivisiblesDe n) == 2 = True
+            | otherwise = False
 
-{-
+{- 
 4)
 Implementar una función en Haskell que reciba un número entero n. La función deberá
 generar una lista con todos los números primos menores o iguales que n. Utilizar lista por
 comprensión. Reutilizar la función de la consigna 2.
 -}
-listaFactoresPrimosDe :: Int -> [Int]
-listaFactoresPrimosDe n = listaPrimos(n, n)
 
+listaPrimosMenorIgualA :: Int -> [Int]
+listaPrimosMenorIgualA n = [x | x <- [1..n], esPrimo(x)]
 
-listaPrimos :: (Int, Int) -> [Int]
-listaPrimos(a, b)	| (b < 2) == True							= []
-					| (esPrimo b && esDivisible(a, b)) == True 	= b:listaPrimos(a, b-1)
-					| (esPrimo b && esDivisible(a, b)) == False = listaPrimos(a, b-1)
-					| otherwise = []
 {-
 5)
 Implementar una función en Haskell que reciba un número entero n. La función deberá
 generar una lista con todos los factores primos del número n. Es decir, en este caso, el producto de
 todos los factores de la lista a generar deberá ser igual a n. Reutilizar las funciones necesarias.
 -}
-factorizar :: (Int) -> [Int]
-factorizar (n)  | (n < 2) == True = []
-				| (length(listaFactoresPrimosDe n) > 0) == True = last(listaFactoresPrimosDe n):factorizar ( n `div` last(listaFactoresPrimosDe n))
-				| otherwise = []
-						      
+
+listaFactoresPrimosDe :: Int -> [Int]
+listaFactoresPrimosDe n = [x | x <- listaPrimosMenorIgualA n, esDivisible(n, x)]
+
+primerFactorPrimoDe :: Int -> Int
+primerFactorPrimoDe n = head(listaFactoresPrimosDe n)
+
+primeraFactorizacion :: Int -> Int
+primeraFactorizacion n = div n (primerFactorPrimoDe n)
+
+factorizar :: Int -> [Int]
+factorizar n | n < 2 = []
+             | otherwise = primerFactorPrimoDe n:factorizar (primeraFactorizacion n)
+                              
 {-
 6)
 Implementar una función en Haskell que reciba una lista de tuplas. Cada tupla de la
@@ -74,7 +80,6 @@ correspondiente a elevar la base (primer elemento de la tupla) al exponente de d
 elemento de la tupla). Y a su vez, multiplicar todas las potencias obtenidas entre sí.
 -}
 
-
 expandir :: [(Int, Int)] -> Int
-expandir list 	| (length (list) < 1) == True = 1
-				| otherwise = fst (head(list)) ^ snd (head(list)) * expandir (tail(list))
+expandir [] = 1
+expandir (h:t) = fst h ^ snd h * expandir t
